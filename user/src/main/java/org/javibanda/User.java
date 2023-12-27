87e5036e3424175cc6d7dcb2c95dda89844fbc92
javibanda <narutojavibanda@gmail.com>
2023-12-27 22:37:01 +0100

    Add user micro

---
+++ user/src/main/java/org/javibanda/User.java
@@ -0,0 +1,11 @@
+package org.javibanda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class User {
+    public static void main(String[] args) {
+        SpringApplication.run(User.class);
+    }
+}
