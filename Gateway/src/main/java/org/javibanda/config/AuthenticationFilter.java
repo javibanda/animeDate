54773519ff16b3f1ae73a2c8d6eeff156042d21e
javibanda <narutojavibanda@gmail.com>
2023-12-27 23:13:26 +0100

    Add gateway micro

---
+++ Gateway/src/main/java/org/javibanda/config/AuthenticationFilter.java
@@ -0,0 +1,52 @@
+package org.javibanda.config;
+
+
+import lombok.RequiredArgsConstructor;
+import org.javibanda.service.JwtUtil;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+
+@RefreshScope
+@Component
+@RequiredArgsConstructor
+public class AuthenticationFilter implements GatewayFilter {
+
+    private final RouterValidator validator;
+    private final JwtUtil jwtUtil;
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        ServerHttpRequest request = exchange.getRequest();
+
+        if (validator.isSecured.test(request)) {
+            if (authMissing(request)) {
+                return onError(exchange);
+            }
+
+            final String token = request.getHeaders().getOrEmpty("Authorization").get(0);
+
+            if (jwtUtil.isExpired(token)) {
+                return onError(exchange);
+            }
+        }
+        return chain.filter(exchange);
+    }
+
+    private Mono<Void> onError(ServerWebExchange exchange) {
+        ServerHttpResponse response = exchange.getResponse();
+        response.setStatusCode(HttpStatus.UNAUTHORIZED);
+        return response.setComplete();
+    }
+
+    private boolean authMissing(ServerHttpRequest request) {
+        return !request.getHeaders().containsKey("Authorization");
+    }
+}
